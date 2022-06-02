441504ec23d4db57fa0419e5bdf475d2036d811a
cristyan.alexandrino <cristyanalexandrino.od@gmail.com>
2022-06-01 21:51:35 -0300

    Metodo Obter hora por extenso primeiro metodo concluido sem validações

---
+++ TrabalhoOrientacaoObjetos01/TrabalhoOrientacaoObjetos01.Tests/Questao03/ExecutarRelogio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TrabalhoOrientacaoObjetos01.TrabalhoOrientacaoObjetos01.Tests.Questao03
+{
+    public class ExecutarRelogio
+    {
+        [Theory]
+        [InlineData()]
+    }
+}
