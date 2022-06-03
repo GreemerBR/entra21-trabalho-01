047a3fb0a181cbe7b52f812bd8140d1f3fab0beb
Juliana <julianaforbicii>
2022-06-03 13:40:40 -0300

    Feito MENU e testes dos dias.

--- TrabalhoOrientacaoObjetos01.Tests/Questao02/CalendarioTests.cs
+++ TrabalhoOrientacaoObjetos01.Tests/Questao02/CalendarioTests.cs
@@ -36,4 +36,50 @@ public class CalendarioTests
         //Assert
         mesTexto.Should().Be(mesPorExtenso);
     }
+    [Theory]
+    [InlineData(01, "Primeiro")]
+    [InlineData(02, "Dois")]
+    [InlineData(03, "Três")]
+    [InlineData(04, "Quatro")]
+    [InlineData(05, "Cinco")]
+    [InlineData(06, "Seis")]
+    [InlineData(07, "Sete")]
+    [InlineData(08, "Oito")]
+    [InlineData(09, "Nove")]
+    [InlineData(10, "Dez")]
+    [InlineData(11, "Onze")]
+    [InlineData(12, "Doze")]
+    [InlineData(13, "Treze")]
+    [InlineData(14, "Catorze")]
+    [InlineData(15, "Quinze")]
+    [InlineData(16, "Dezesseis")]
+    [InlineData(17, "Dezessete")]
+    [InlineData(18, "Dezoito")]
+    [InlineData(19, "Dezenove")]
+    [InlineData(20, "Vinte")]
+    [InlineData(21, "Vinte e Um")]
+    [InlineData(22, "Vinte e Dois")]
+    [InlineData(23, "Vinte e Três")]
+    [InlineData(24, "Vinte e Quatro")]
+    [InlineData(25,"Vinte e Cinco")]
+    [InlineData(26,"Vinte e Seis")]
+    [InlineData(27,"Vinte e Sete")]
+    [InlineData(28,"Vinte e Oito")]
+    [InlineData(29,"Vinte e Nove")]
+    [InlineData(30,"Trinta")]
+    [InlineData(31,"Trinta e Um")]
+
+     public void Cenario02_Validar_DiaPorExtenso(int diaInfomardo, string diaPorExtenso)
+    {
+        //Arrange
+        var caledario = new Calendario();
+        caledario.Data = DateTime.Today.AddDays(diaInfomardo);
+
+
+        //Act
+        var diaTexto = caledario.ObterDiaPorExtenso();
+
+        //Assert
+        diaTexto.Should().Be(diaPorExtenso);
+    }
 }
--- TrabalhoOrientacaoObjetos01/Questao02/ExecutarCalendario.cs
+++ TrabalhoOrientacaoObjetos01/Questao02/ExecutarCalendario.cs
@@ -10,29 +10,77 @@ namespace TrabalhoOrientacaoObjetos01.TrabalhoOrientacaoObjetos01.Questao02
     {
         public void Executar()
         {
+             var opcaoDesejada = 0;
 
-            //ar dataValida = new verificarData();
-            Console.Write("Informe uma data (aaaa/mm/de)");
-            var data = Console.ReadLine();
+            while (opcaoDesejada != 5)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(@"
+------------------MENU------------------
+1 - Obter hora por extenso
+2 - Obter minuto por extenso
+3 - Obter segundo por extenso
+4 - Obter hora completo por extenso
+5 - SAIR
+");
 
-            // while (dataValida == false)
-            //{
-            //  try
-            //{
-            //    Console.Write("Por favor, informe uma data de 1970 até 2030: ");
-            //  dataValida = Convert.ToDouble(Console.ReadLine());
+                try
+                {
+                    Console.Write("Digite uma opção: ");
+                    opcaoDesejada = Convert.ToInt32(Console.ReadLine());
 
-            //     dataValida = true;
-            // }
-            // catch (Exception ex)
-            //{
-          //Console.ForegroundColor = ConsoleColor.Red;
-       //   Console.WriteLine("A data informada não é válida.\n Informe uma data de 1970 até 2030.");
-            //   }
-            //   }
+                    if (opcaoDesejada < 0 || (opcaoDesejada != 1 && opcaoDesejada != 2 && opcaoDesejada != 3 && opcaoDesejada != 4 && opcaoDesejada != 5))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Por favor, informe um número presente no Menu.")
+                    }
+                    else
+                    {
+                        Console.Clear();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Informe um número presente no Menu.");
+                }
 
-            //   Console.Clear();
-            //}
+                if (opcaoDesejada == 1)
+                {
+                    Console.Clear();
+                    var diaExtenso = Calendario.ObterDiaPorExtenso();
+                    Console.WriteLine($"Dia por extenso: {diaExtenso}");
+                    Console.WriteLine(diaExtenso);
+                }
+
+                if (opcaoDesejada == 2)
+                {
+                    Console.Clear();
+                    var mesExtenso = Calendario.ObterMesPorExtenso();
+                    Console.WriteLine($"Mês por extenso: {mesExtenso}");
+                    Console.WriteLine(mesExtenso);
+                }
+
+                if (opcaoDesejada == 3)
+                {
+                    Console.Clear();
+                    var anoExtenso = Calendario.ObterAnoPorExtenso();
+                    Console.WriteLine($"Ano por extenso: {anoExtenso}");
+                    Console.WriteLine(anoExtenso);
+                }
+
+                if (opcaoDesejada == 4)
+                {
+                    Console.Clear();
+                    var dataCompletaPorExtenso = Calendario.ObterDataCompletaPorExtenso();
+                    Console.WriteLine($"Data completa por extenso: {dataCompletaPorExtenso}");
+                    Console.WriteLine(dataCompletaPorExtenso);
+                }
+
+                        Console.Write("Informe uma data (aaaa/mm/de)");
+                         var data = Console.ReadLine();
+
+           
         }
 
     }
